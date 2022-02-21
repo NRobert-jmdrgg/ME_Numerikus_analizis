b4a7bb48aaf867a37b395a1f967e54cf8ba97fc4
NRobert-jmdrgg <rob.iskola1@gmail.com>
2022-02-21 23:18:52 +0100

    Update max_in_range.m

--- 3_het/max_in_range.m
+++ 3_het/max_in_range.m
@@ -1,6 +1,6 @@
 function max = max_in_range(func, a, b, n)
     func = diff(func, n);
-    max = subs(func(), a);
+    max = abs(subs(func(), a));
     for i = a + 1 : b
         if abs(subs(func(), i)) > max
             max = abs(subs(func(), i));
